4e94d740810a1a272f490ecd387680521d22d7e3
alvmornav <alvmornav@alum.us.es>
2025-10-17 20:03:57 +0200

    Corrigiendo errores

--- front/svelte.config.js
+++ front/svelte.config.js
@@ -12,18 +12,16 @@ const config = {
 					'self',
 					'unsafe-inline',               // sigue permitiendo estilos inline
 					'https://cdn.jsdelivr.net',    // tu CDN habitual
-					'https://sos2425-13.onrender.com' // tu dominio de Render
+					'https://sos2425-oct-amn.onrender.com' // tu dominio de Render
 				],
 				'img-src': ['self', 'data:', 'https:'],
 				'connect-src': [
 					'self',
 					'http://localhost:16078',
-					'https://sos2425-13.onrender.com',
-					'https://sos2425-10.onrender.com',
-					'https://sos2425-12.onrender.com',
-					'https://sos2425-14.onrender.com',
-					'https://sos2425-20.onrender.com',
-					'https://sos2425-21.onrender.com'
+					'https://sos2425-crr.onrender.com',         
+                    'https://sos2425-15.onrender.com',
+                    'https://sos2425-20.onrender.com', 
+                    'https://sos2425-agb.onrender.com'
 				]
 			}
 		}
